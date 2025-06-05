c135cd75f243fde67ffbc1e28b0bc78e310a8a68
leanderfr <leanderprogrammer@gmail.com>
2025-06-05 12:41:44 -0300

    Atl

---
+++ frontend_vue/src/App.vue
@@ -0,0 +1,262 @@
+
+<template>
+
+  <div>  <!-- exigencia do Vue, tudo agrupado em 1 elemento -->
+
+    <div class='appBody'>  
+
+      <div class='headerBar'>
+
+        <div id='headerLogo' ></div>
+
+        <div class='headerText' >
+            <div>Avaliação PHP / Laravel</div>
+        </div>
+
+        <!-- seletor de backend  -->
+        <div class="headerRight">    
+
+          <div :class="! backendChanged ? 'backendClicked' : 'backendUnclicked' "   id='backendPHP'  @click="backendChanged = false"  >         
+            <img src="./assets/images/php.svg" alt='' />
+          </div>
+
+          <label for="chkBackendSelector" class="switch_backend"  >
+            <input id="chkBackendSelector" type="checkbox"  v-model="backendChanged"    />
+            <span class="slider_backend round"></span>
+          </label>
+
+          <div :class="backendChanged ? 'backendClicked' : 'backendUnclicked' "   id='backendLaravel'  @click="backendChanged = true"  >         
+            <img src="./assets/images/laravel.svg" alt='' />
+          </div>
+
+        </div>
+
+      </div>
+
+      <!-- menu superior -->
+      <div class='menuSuperior' id='menuSuperior' style='scrollbar-width: thin;' @wheel='toWheelMenu'>
+        <MenuSuperior 
+          :selectedItem='selectedMenu' 
+          :backendUrl='backendUrl'    
+          @setNewselectedMenu='setNewselectedMenu'            /> 
+      </div>
+
+
+      <!-- mainContainer=> conteudo dinamico, depende do que o usuario quer ver no momento (datatable ou eventuais outros componentes) -->
+      <div id='mainContainer'  >
+
+
+          <!-- mostra datatable se usuario pediu para ver alguma tabela  -->
+          <div v-if='toDisplayDatatable'>
+
+            <Datatable  
+                :key='toRefreshDatatable'
+                :currentViewedDatatable = currentViewedDatatable
+                :setDatatableToDisplay='setDatatableToDisplay' 
+                :expressions='expressions' 
+                :currentBackend="backendChanged ? 'usa' : 'brazil'" 
+                :backendUrl='backendUrl'    
+                @showLoading="isLoading=true" 
+                @hideLoading="isLoading=false"     
+                @setDatatableToDisplay='setDatatableToDisplay'  />
+          </div>
+
+
+      </div>
+
+      <!-- canto inferior   -->
+      <div class='bottomToolbar'  >     
+      </div>
+
+    </div>
+
+
+
+    <!-- animacao 'carregando...' exibida qdo frontend aguardando resposta do backend  -->
+    <div v-show="isLoading" class='backdropTransparent'  >
+      <div id='divLoading' >&nbsp;</div>
+    </div>
+
+    <!-- mensagem rolante (horizontal) para avisar erro/sucesso em operacoes -->
+    <div id="messagesSlidingDiv" >
+      &nbsp;
+    </div>
+
+    <audio id="alertBeep" >
+      <source src="./assets/sounds/error_beep.mp3" type="audio/mpeg">    
+    </audio>
+
+    <!-- icone cachorrinho , canto inferior direito  -->
+    <div class='_doggy'  id='divDoggy'></div>
+    <div class='_doggy_1' id='divDoggy_1'></div>
+    <div class='_doggy_2' id='divDoggy_2'></div>
+    <div class='_doggy_3' id='divDoggy_3'></div>
+
+    <div class='absolute bottom-0  flex flex-row left-4 text-[20px] font-bold gap-7 h-[50px]'>
+      <div class='flex flex-row items-center gap-3' >
+        Frontend:
+        <img src="./assets/images/vue.svg" alt='' />
+      </div>
+
+    </div>
+
+
+
+  </div>
+
+
+</template>
+
+
+<!-- composition API , way cleaner then options API -->
+<script setup>
+  import { ref, onMounted, watch, onBeforeMount  } from 'vue';
+  import MenuSuperior from './MenuSuperior.vue';
+  import Datatable from './components/Datatable.vue';
+
+  // alguns efeitos jquery 
+  import 'jquery-ui-bundle';
+  import 'jquery-ui-bundle/jquery-ui.min.css';
+
+  import { toWheelMenu, prepareLoadingAnimation, slidingMessage , preparePuppyIcon, loadScripts } from './assets/js/utils.js'
+
+  const neededJsLoaded = ref(false)   
+
+  const toRefreshDatatable = ref(0)  
+
+  const backendChanged = ref(true)
+  const isLoading = ref(true)
+  const error = ref(null)
+
+  // backend depende da escolha do usuario e se esta rodando local ou na AWS
+  //const backendUrl = ref('http://ec2-54-233-183-5.sa-east-1.compute.amazonaws.com:8073')  
+  const backendUrl = ref('http://localhost')  
+
+  // opcao do menu atual
+  const selectedMenu = ref(0)
+
+  // qual tabela deve ser exibida no momento
+  const currentViewedDatatable = ref('')
+
+  // se é hora de exibir texto sobre a aplicação
+  const toDisplayAbout = ref(true)
+
+  // se é hora de exibir uma tabela
+  const toDisplayDatatable = ref(false)
+
+
+
+
+
+  //***************************************************************************
+  // usuario escolheu opcao no menu (top)
+  //***************************************************************************
+  const setNewselectedMenu = (itemMenu) => {
+    selectedMenu.value = itemMenu
+  }
+
+  //***************************************************************************
+  // usuario escolheu alguma tabela pra ser exibida
+  //***************************************************************************
+  const setDatatableToDisplay = (datatable) => {
+    toDisplayAbout.value = false;
+    toDisplayDatatable.value = true;
+
+    toRefreshDatatable.value++  // força re-render da tabela escolhida
+
+    currentViewedDatatable.value = datatable
+  }
+
+    //***************************************************************************
+  //***************************************************************************
+  onMounted( () => {
+      // handler do teclado, facilita vida do usuario, exemplo: F2= grava registro, Enter= avança campo, etc
+      window.onkeydown = (event) => {
+        onKeyDown(event)
+      };
+
+
+      // nao consigo preparar a aplicacao pra ser responsiva agora, so tenho 24h pra entregar o teste
+      // se usuario redimensionar, fecha qq modal e redesenha tela
+      window.onresize = () => {
+        setTimeout(() => {
+          window.location.reload();  
+        }, 500);
+      }
+ 
+      preparePuppyIcon()
+      prepareLoadingAnimation()
+
+      isLoading.value = true
+
+  })
+
+  //***************************************************************************
+  // se usuario alterar backend atual, força atualizacao da datatable atual
+  // isso se alguma datatable estiver sendo exibida, claro
+  //*************************************************************************** 
+  watch([backendChanged], () => { 
+      isLoading.value = true;
+
+      Promise.all( [fetchExpressions()] ).then(() => {
+        isLoading.value = false
+        // componentes afetados
+        toRefreshDatatable.value++
+        toRefreshSchedule.value++
+      })        
+    },
+    { immediate: false }
+  )
+
+
+
+
+/************************************************************************************************************************************************************
+facilita vida do usuario com atalhos teclado
+F2= grava registro, Esc= fecha modal, etc
+************************************************************************************************************************************************************/
+
+const onKeyDown = (e) =>  {
+
+  // usuario pressionou Enter ou setas acima/abaixo, avança/retrocede campo do form atual
+  if ( (e.which == 13 || e.which == 38 || e.which == 40)  && $('.text_formFieldValue').is(':focus') )   { 
+        // tive que inventar a propriedade 'sequence', porque o VITE reclama muito em relacao a tabIndex
+
+        let tab =  $(':focus').attr("sequence");
+        if (e.which==13 || e.which == 40)  tab++;
+        else if (e.which==38)  tab--;
+
+        e.preventDefault()
+
+        // coloca foco no campo anterior/proximo
+        $("[sequence='"+tab+"']").focus();          
+  }
+
+  // usuario pressionou Enter com a caixa de pesquisa (datatable) focada, dispara pesquisa 
+  if (e.which == 13 && $('#txtTableSearchText').is(':focus') )   { 
+    if (  $.trim($('#txtTableSearchText').val()).length<3 ) 
+      slidingMessage('Mínimo 3 caracteres', 2000)        
+    else 
+      $('#triggerSearchBox').click()
+  } 
+    
+  
+  // usuario pressionou F2 com algum form edicao aberto, dispara gravacao
+  // pressionou Esc, fecha qq modal
+  if (e.which == 27 || e.which == 113)   { 
+        let editionForm = typeof $('#fornecedorForm').attr("id")!='undefined' || 
+                          typeof $('#usuarioForm').attr("id")!='undefined' 
+
+        // dispara botao 'fechar'
+        if (editionForm)  {
+          if (e.which == 27)   $('#btnCLOSE').trigger('click')
+
+          // 'F2= salvar'
+          if (e.which == 113)   $('#btnSAVE').trigger('click')   // f2 was pressed
+        }
+  }
+}
+
+
+</script>
+
---
+++ frontend_vue/src/assets/js/utils.js
@@ -0,0 +1,203 @@
+
+import 'spin.js/spin.css';
+import {Spinner} from 'spin.js';
+
+/************************************************************************************************************************************************************
+prepara animacao 'carregando...'
+************************************************************************************************************************************************************/
+
+const prepareLoadingAnimation = () => {
+    var opts = {
+      lines: 12 // linhas desenhar
+    , length: 30 // largura cada linha
+    , width: 18 // espessura linha
+    , radius: 42 // raius do circulo
+    , scale: 0.3 // tamanho do spinner
+    , corners: 3 // contorno do canto
+    , color: 'gray' 
+    , opacity: 0.3 
+    , rotate: 0 
+    , direction: 1 // 1: sentido horario, -1: ñ horario
+    , speed: 1 
+    , trail: 60 // birlho
+    , fps: 20 // Frames por segundo
+    , zIndex: 2e9 // acima de qq div
+    , className: 'spinner' 
+    , top: '50%' // posicao em relacao ao parent
+    , left: '50%' // posicao em relacao ao parent
+    , shadow: true // sombra?
+    , hwaccel: true // aceleracao de hardwar5e
+    , position: 'absolute' 
+    ,animation: 'spinner-line-fade-quick'
+    }
+
+    // para exibir/ocultar esta div, usar as funcoes: showLoadingGif()/hideLoadingGif()
+    var divLoading = document.getElementById('divLoading');
+    new Spinner(opts).spin(divLoading);
+}
+
+/************************************************************************************************************************************************************
+string esta formato json?
+************************************************************************************************************************************************************/
+const  isStringJSON = (string) => {
+    try {
+        JSON.parse(string);
+    } catch (e) {
+        return false;
+    }
+    return true;
+
+}
+
+
+
+/************************************************************************************************************************************************************
+mensagem rolante (horitontal) de alerta
+************************************************************************************************************************************************************/
+const slidingMessage = (html, time) => {
+
+  let slidingDIV = $('#messagesSlidingDiv')
+
+  slidingDIV.html('&nbsp;&nbsp;&nbsp;&nbsp;' + html);
+  slidingDIV.show("slide", { direction: "left" }, 200);
+
+  // navegador nao vai tocar beep a nao ser que usuario tenha clicado em algo, questao de segurança
+  if (navigator.userActivation.hasBeenActive)     $('#alertBeep')[0].play()
+
+  setTimeout(function () { slidingDIV.hide("slide", { direction: "right" }, 200); }, time);
+}
+
+/************************************************************************************************************************************************************
+contador, util para loops 'v-for' do Vue
+************************************************************************************************************************************************************/
+const counter = (start, end) => {
+    const result = [];
+    for (let i = start; i <= end; i++) {
+      result.push(i);
+    }
+    return result;
+}
+
+//********************************************************************************************************************************
+// prepara mouseenter do icone cachorrinho (canto inferior direito)
+//*******************************************************************************************************************************
+
+const preparePuppyIcon = () => {
+  $('#divDoggy').mouseover(function (e) {
+    $('#divDoggy_1').show(); $('#divDoggy_2').show(); $('#divDoggy_3').show();
+    $('#divDoggy_1').animate({ bottom: '55px', right: '105px', zIndex: 3000 }, 200, function () {
+    });
+
+    $('#divDoggy_2').animate({ bottom: '75px', right: '125px', zIndex: 3000 }, 200);
+    $('#divDoggy_3').animate({ bottom: '90px', right: '105px' }, 200, function () {
+      $(this).css('z-index', 2101);
+    });
+  });
+
+  // usuario tirou  mouse do icone cachorro 
+  $('#divDoggy').mouseout(function (e) {
+    $('#divDoggy_1').hide(); $('#divDoggy_2').hide(); $('#divDoggy_3').hide();
+    $('#divDoggy_1').css('right', '80px'); $('#divDoggy_1').css('bottom', '1px');
+    $('#divDoggy_2').css('right', '80px'); $('#divDoggy_2').css('bottom', '1px');
+    $('#divDoggy_3').css('right', '80px'); $('#divDoggy_3').css('bottom', '-150px');
+  });
+}
+
+
+/***********************************************************************************************************************
+torna uma div arrastavel pelo mouse
+***********************************************************************************************************************/
+const makeWindowDraggable = (title_id, window_id) => {
+  $(`#${window_id}`).draggable({ handle: `#${title_id}`, containment: '#backDrop' });
+}
+
+/***********************************************************************************************************************
+a tooltip jquery nao some quando clica se sobre o elemento, necessario fazer a tooltip sumir via codigo
+***********************************************************************************************************************/
+const forceHideToolTip = () => {
+  $('div[class^="ui-tooltip"]').remove();
+}
+
+/*******************************************************************************/
+// verifica se um elemento ainda esta visivel na viewport
+// util quando reexibindo uma div 'row' de uma datatable que foi recem alterada
+/*******************************************************************************/
+const divStillVisible = (divId) => {
+    // distancia topo da pagina
+    var windowScrollTopView = $(window).scrollTop();
+    
+    // topo pagina + altura video
+    var windowBottomView = windowScrollTopView + $(window).height();
+ 
+    let $div = $(divId)
+    
+    // distancia elemento do topo
+    var elemTop = $div.offset().top;
+            
+    let elemBottom = elemTop + $div.height() + 80; 
+      return ( elemBottom <= windowBottomView) && (elemTop >= windowScrollTopView ) ;
+}
+
+
+
+/***********************************************************************************************************************************************************/
+// rola uma div ate reexibir determinado elemento dentro dela
+// util quando reexibindo uma div 'row' de uma datatable que foi recem alterada
+/***********************************************************************************************************************************************************/
+export const scrollUntilElementVisible = (element_id) => {
+
+  if (typeof $("#" + element_id).attr("id") == "undefined") return; // verifica existe
+
+  let container_div = $("#" + element_id).scrollParent(); // acha o parent
+
+  // elemento obrigatoriamente tem que ter ID
+  let element = document.getElementById(element_id);
+
+  element.scrollIntoView();
+
+  let posY = container_div.scrollTop();
+  container_div.scrollTop(posY - 15); // rola um pouco pra cima porque scrollIntoView exagera quando desce
+}
+
+
+
+//************************************************************************************************************
+// carrega arquivos JS um apos o outro, pra evitar chamar uma funcao nao carregada ainda
+//************************************************************************************************************
+  function loadScripts(scripts) {
+      var promises = [];
+      scripts.forEach(function(script) {
+          promises.push($.getScript(script));
+      });
+      return $.when.apply($, promises);
+  }
+
+//******************************************************************************
+// melhora o visual da tooltip (title) 
+// elementos que tem a classe indicativa (sem css valido) 'putPrettierTooltip'
+//******************************************************************************
+function improveTooltipLook() {
+
+  setTimeout( () => {
+    if (typeof $('.putPrettierTooltip').tooltip !== "undefined") {
+      $('.putPrettierTooltip').tooltip({ 
+        tooltipClass: 'prettierTitle_black',  
+        show: false,  
+        hide: false,  
+        position: { my: "left top", at: "left top-40", collision: "flipfit" }
+      })
+    }
+
+  }, 500)    
+
+
+}
+
+
+
+//********************************************************************************************************************************
+//*******************************************************************************************************************************
+export { prepareLoadingAnimation, isStringJSON, slidingMessage, counter, makeWindowDraggable, divStillVisible, 
+      hourFormat, preparePuppyIcon, dateToIsoStringConsideringLocalUTC, formatDate, forceHideToolTip, loadScripts,
+      toWheelMenu, improveTooltipLook };
+
